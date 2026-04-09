565f453db3fa3c3cd229f4939a9df9c7a999830b
GioEspinoza <giovanni.espinoza0@gmail.com>
2026-04-09 00:03:21 -0400

    Added persistent data logic, working on local one user password system, JSON logic

---
+++ backend/storage.py
@@ -0,0 +1,27 @@
+import json
+def save_data(tasks):
+    """save data to json"""
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+def load_data():
+    """load data from json"""
+    try:
+        with open ("tasks.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+def save_name(username, password):
+    data = {
+        "username":username,
+        "password":password
+    }
+    with open("user_prof.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+def load_user_prof():
+    try:
+        with open("user_prof.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
