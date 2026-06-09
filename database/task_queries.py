7377ffd13750a55f260b903b0a88dc3cc669a38f
GioEspinoza <giovanni.espinoza0@gmail.com>
2026-06-09 14:00:29 -0400

    task query file to send task data into json

---
+++ database/task_queries.py
@@ -0,0 +1,27 @@
+from database.db import get_db_connection
+
+def add_task(user_id, task_name, course_name=None, due_date=None, difficulty=None, estimated_hours=None):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO tasks (
+                    user_id,
+                    task_name,
+                    course_name,
+                    due_date,
+                    difficulty,
+                    estimated_hours
+                )
+                VALUES (%s, %s, %s, %s, %s, %s)
+                RETURNING task_id;
+                """,
+                (user_id, task_name, course_name, due_date, difficulty, estimated_hours)
+            )
+
+            row = cur.fetchone()
+
+            if row is None:
+                raise ValueError("Task insert failed. No task_id returned.")
+ 
+            return row[0]
